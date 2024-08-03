73a25f63fdd09eabad9abd8e475eaf318f3accd2
brunojaciel <bruno_mello@edu.univali.br>
2024-08-03 10:42:51 -0300

    model trained

--- train.py
+++ train.py
@@ -4,4 +4,4 @@ model = YOLO("yolov8n-pose.pt")
 
 dataset_location = "/home/zeus/Área de Trabalho/TCC/yolov8_pose/dataset/data.yaml"
 
-results = model.train(data=dataset_location, epochs=100, imgs=640)
+results = model.train(data=dataset_location, epochs=100, imgsz=640)
